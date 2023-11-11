ede1d4d031ad62abed13bed3697bbe9b798ce94c
guillaume-kennes-vinci <114008498+guillaume-kennes-vinci@users.noreply.github.com>
2023-11-11 20:48:28 +0100

    add void to no name case and undefined + null case

--- __test__/greet.test.js
+++ __test__/greet.test.js
@@ -2,8 +2,20 @@
 
 const { greet } = require('../utils/utils.js')
 
-test('if no name', () => {
+test('if no name (void)', () => {
   const name = ''
+  const result = greet(name)
+  expect(result).toBe('Hello, my friend.')
+})
+
+test('if no name (undefined)', () => {
+  const name = undefined
+  const result = greet(name)
+  expect(result).toBe('Hello, my friend.')
+})
+
+test('if no name (null)', () => {
+  const name = null
   const result = greet(name)
   expect(result).toBe('Hello, my friend.')
 })
