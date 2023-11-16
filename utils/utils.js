30f5ec34a281dfbea6e43858a5353576d6db561f
guillaume-kennes-vinci <114008498+guillaume-kennes-vinci@users.noreply.github.com>
2023-11-16 11:36:45 +0100

    readd .join to main

--- utils/utils.js
+++ utils/utils.js
@@ -14,7 +14,7 @@ function greet (name) {
 }
 
 function greetArray (names) {
-  const allButLast = names.slice(0, -1)
+  const allButLast = names.slice(0, -1).join(', ')
   const last = names[names.length - 1]
   return 'Hello, ' + allButLast + ' and ' + last + '.'
 }
